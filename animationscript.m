91964aa7deb0393c2fc9880191be39b83d2e531b
arpanrau <Arpan.Rau@gmail.com>
2015-12-03 10:57:54 -0500

    changed animation

--- animationscript.m
+++ animationscript.m
@@ -1,4 +1,4 @@
-function animationscript(L1,L2,T,Y,M1,M2)
+function animationscript(L1,L2,T,Y,M1,M2,step)
 
 %Animates ode45 double pendulum implementation
 %Accepts Length of first leg, length of 2nd leg,
@@ -12,14 +12,18 @@ y1 = -L1*cos(Y(:,1));
 y2 = -L1*cos(Y(:,1))-L2*cos(Y(:,2));
 
 for i = 1:length(T)
+    tic
     clf;
     axis([-(L1 + L2), L1+L2, -(L1+L2),L1+L2])
+    axis square
     hold on;
     plot(x1(i), y1(i), 'r.', 'MarkerSize', M1*10);
     plot(x2(i), y2(i), 'b.', 'MarkerSize', M2*10);
     plot([0 x1(i)], [0 y1(i)], 'r');
     plot([x1(i) x2(i)], [y1(i) y2(i)], 'b');
     drawnow
+    stop = toc;
+    pause(step-stop);
 end
 end
     
--- double_pendulum.m
+++ double_pendulum.m
@@ -1,13 +1,13 @@
 function  double_pendulum()
-L1 = 10;
-L2 = 10;
-M1 = 5;
-M2 = 0.1;
+L1 = .5;
+L2 = .5;
+M1 = 3;
+M2 = 1;
 g = 9.8;
 
-end_time = 100;
-step = 0.1;
-initial = [0.52;0.52;0;0];
+end_time = 10;
+step = 0.05;
+initial = [0.2;3;0;0];
 
 [T,U] = ode45(@netFlow,0:step:end_time,initial);
 
@@ -20,6 +20,31 @@ initial = [0.52;0.52;0;0];
         theta2A = (2*sin(theta1-theta2)*((theta1V^2)*L1*(M1+M2)+g*(M1+M2)*cos(theta1)+(theta2V^2)*L2*M2*cos(theta1-theta2)))/(L2*(2*M1+M2-M2*cos(2*theta1-2*theta2)));
         res = [theta1V; theta2V; theta1A; theta2A];
     end
-animationscript(L1,L2,T,U,M1,M2)
+animationscript(L1,L2,T,U,M1,M2,step)
+y1 = -L1*cos(U(:,1));
+y2 = -L1*cos(U(:,1))-L2*cos(U(:,2));
+
+v1 = U(:,3)*L1;
+v2 =sqrt((U(:,4).*L2.*sin(U(:,2)))+(U(:,3).*L1.*sin(U(:,1))).^2 + ((U(:,4).*L2.*cos(U(:,2)))+(U(:,3).*L1.*cos(U(:,1))).^2)) ;
+
+PE1 = g*M1*(y1)*-1;
+PE2 = g*M2*(y2)*-1;
+KE1 = .5*M1*v1.^2;
+KE2 = .5*M2*v2.^2;
+
+E = PE1 + PE2 + KE1 + KE2;
+
+figure
+plot(T,E);
+hold on
+plot(T,PE1);
+plot(T,PE2);
+plot(T,KE1);
+plot(T,KE2);
+
+legend('E','PE1','PE2','KE1','KE2')
+
+
+
 end
 
